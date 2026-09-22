821ab404fd6b172a817b9f8aa182628207696ec8
AntonioN64 <juan.antonio.najera64@gmail.com>
2026-09-21 20:53:57 -0600

    Subir bien el directorio

---
+++ directorio/src/app/components/controls/controls.ts
@@ -0,0 +1,25 @@
+import { Component, output } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-controls',
+  styleUrl: './controls.css',
+  templateUrl: './controls.html',
+})
+export class Controls {
+  orderByIdClick = output<void>();
+  orderByNameClick = output<void>();
+  reverse = output<void>();
+
+  onOrderByIdClick(): void {
+    this.orderByIdClick.emit();
+  }
+
+  onOrderByNameClick(): void {
+    this.orderByNameClick.emit();
+  }
+
+  onReverseClick(): void {
+    this.reverse.emit();
+  }
+}
